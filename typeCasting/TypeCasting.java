4487c313761a1ee07032e6403d0c72c167035ca9
pravesh negi <praveshnegi143@gmail.com>
2021-06-21 12:05:26 +0530

    TypeCasting

--- typeCasting/TypeCasting.java
+++ typeCasting/TypeCasting.java
@@ -3,9 +3,13 @@ package typeCasting;
 public class TypeCasting {
     public static  void main(String[] args)
     {
-        byte x=4;
-        int y=x;
+//        byte x=4;
+//        int y=x;
+//        System.out.println(y);
+        double x=2.2463;
+        int y=(int)x;
         System.out.println(y);
+
 
     }
 }
