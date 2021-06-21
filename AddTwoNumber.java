f92a0348443213bd96b86d7e8de1f3e47659b296
pravesh negi <praveshnegi143@gmail.com>
2021-06-21 11:40:01 +0530

    Sum

---
+++ AddTwoNumber.java
@@ -0,0 +1,10 @@
+public class AddTwoNumber {
+    public static void main(String[] args)
+    {
+        int num1=5;
+        int num2=7;
+        int sum;
+        sum=num1+num2;
+        System.out.println(sum);
+    }
+}
