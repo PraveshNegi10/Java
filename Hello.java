e6b7d300714af25fc90b15a78c3f4a83d1a7291b
pravesh negi <praveshnegi143@gmail.com>
2021-06-21 09:57:31 +0530

    Testing

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
-package HelloWorld;
+
 public class Hello
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello");
     }
 }
