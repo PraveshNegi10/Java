794481ea2268d44a945a91d22194fcfd75b4577c
pravesh negi <praveshnegi143@gmail.com>
2021-06-23 23:48:40 +0530

    OOPS basic Concept

---
+++ OOPS/MainClass.java
@@ -0,0 +1,25 @@
+package OOPS;
+class cat{
+    boolean furr;
+    String breed,color;
+    int legs,eyes;
+    public void walk()
+    {
+        System.out.println("Cat is walking:");
+    }
+    public void eat()
+    {
+        System.out.println("Cat is eating:");
+    }
+
+}
+
+public class MainClass {
+    public static void main(String[] args)
+    {
+        cat cat1 = new cat();
+        cat cat2 = new cat();
+        cat1.walk();
+        cat2.eat();
+    }
+}
