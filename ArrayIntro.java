a3db327ed1124ca27213324a1d1e7dcb936f26a2
pravesh negi <praveshnegi143@gmail.com>
2021-06-23 22:14:32 +0530

    Array Basics

--- ArrayIntro.java
+++ ArrayIntro.java
@@ -4,7 +4,7 @@ public class ArrayIntro {
     {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int [] arr = new int[n];
+        int arr []  = new int[n];
         for(int i=0;i<n;i++)
         {
             arr [i] = sc.nextInt();
